3b9ede9554a3cb2cae2e8a0a6a7795e53b92d9c5
Philippa Choong <pippachoong@gmail.com>
2022-10-12 23:14:27 +1100

    add to get updates

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const cors = require('cors')
 const app = express()
 const PORT = 3000
 
-app.use( cors() )
+app.use(cors())
 
 // To get access to POSTed 'formdata' body content, we have to 
-app.use( express.json() )
-app.use( express.urlencoded({ extended: true}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT} ...`)
